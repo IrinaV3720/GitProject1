8a9963d4156327521a8acbd595cc101879739a2d
IrinaV3720 <iraalazarenko@gmail.com>
2022-01-04 22:24:40 -0600

    Untracked by mistake//recommiting one more time

---
+++ src/work1/Hello.java
@@ -0,0 +1,10 @@
+package work1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Created Git Repository");
+    }
+}
